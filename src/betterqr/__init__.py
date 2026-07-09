ea535c35927dff9b34062cc392ed6ed04b28964b
DevX_Dragon <218102295+DevX-Dragon@users.noreply.github.com>
2026-07-09 22:39:14 +0530

    chore: Modify hardcoded version

--- src/betterqr/__init__.py
+++ src/betterqr/__init__.py
@@ -1,6 +1,9 @@
 from .core import (
-    QR, WiFi, VCard, MeCard, GeoLocation,
+    QR, save, WiFi, VCard, MeCard, GeoLocation,
     Event, SMS, Email, Phone, Crypto, batch
 )
-__version__ = "1.0.0"
-__all__ = ["QR","WiFi","VCard","MeCard","GeoLocation","Event","SMS","Email","Phone","Crypto","batch"]
+__version__ = "2.0.0"
+__all__ = [
+    "QR", "save", "WiFi", "VCard", "MeCard", "GeoLocation",
+    "Event", "SMS", "Email", "Phone", "Crypto", "batch",
+]
