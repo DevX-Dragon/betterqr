f0aa97c650f2c59ef7d204ea8259d09cdeb8def0
DevX_Dragon <218102295+DevX-Dragon@users.noreply.github.com>
2026-07-15 22:42:22 +0530

    feat: add structured append

--- src/betterqr/__init__.py
+++ src/betterqr/__init__.py
@@ -1,9 +1,9 @@
 from .core import (
-    QR, save, WiFi, VCard, MeCard, GeoLocation,
+    QR, save, structured_append, WiFi, VCard, MeCard, GeoLocation,
     Event, SMS, Email, Phone, Crypto, batch
 )
 __version__ = "2.0.0"
 __all__ = [
-    "QR", "save", "WiFi", "VCard", "MeCard", "GeoLocation",
+    "QR", "save", "structured_append", "WiFi", "VCard", "MeCard", "GeoLocation",
     "Event", "SMS", "Email", "Phone", "Crypto", "batch",
 ]
